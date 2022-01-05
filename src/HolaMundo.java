100b149259c43a60973d3f62a726537fb32d392f
NicolasCaceres <nicolascaceres@MacBook-Air-de-NicolasCaceres.local>
2022-01-05 08:27:14 -0500

    second commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("hola mundo \uD83D\uDE8C");
+        System.out.println("hola mundo2 \uD83D\uDE8C");
+    }
+}
